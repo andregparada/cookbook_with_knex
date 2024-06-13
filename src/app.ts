262d3259cef379596e0cf4011ba00fc8415c2111
André <andregparada@gmail.com>
2024-06-13 13:33:26 -0300

    feat: handler de erro no app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
 import { usersRoutes } from './routes/users'
+import { mealsRoutes } from './routes/meals'
+import { env } from './env'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
@@ -9,6 +12,22 @@ app.register(cookie)
 app.register(usersRoutes, {
   prefix: 'users',
 })
-app.register(usersRoutes, {
+app.register(mealsRoutes, {
   prefix: 'meals',
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO Here we should log to an internal tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
